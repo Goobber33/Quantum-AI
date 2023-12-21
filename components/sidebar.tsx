43d37c804ff3d89c72d95f84ef0d89b206275f88
Kyle <kcparks1234@gmail.com>
2023-12-20 19:10:03 -0800

    Added route colors for my sidebar

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -4,11 +4,21 @@ import Link from "next/link";
 import Image from "next/image";
 import { Montserrat } from "next/font/google";
 import { cn } from "@/lib/utils"
+import { LayoutDashboard } from "lucide-react";
 
 const montserrat = Montserrat({
     weight: "600",
     subsets: ["latin"]
 });
+
+const routes = [
+    {
+        label: "Dashboard",
+        icon: LayoutDashboard,
+        href: "/dashboard",
+        color: "text-sky-500",
+    }
+];
 
 const Sidebar = () => {
     return (
@@ -23,9 +33,19 @@ const Sidebar = () => {
                                 src="/logo.png"
                             />
                         </div>
-                        <h1 className={cn ("text-2xl font-bold", montserrat.className)}>Genius</h1>
+                        <h1 className={cn("text-2xl font-bold", montserrat.className)}>Genius</h1>
                     </div>
                 </Link>
+                <div className="space-y-1">
+                    {routes.map((route) => (
+                        <Link href={route.href} key={route.href}>
+                            <div className="flex items-center flex-1">
+                                <route.icon className={cn("h-5 w-5 mr-3", route.color)} />
+                                {route.label}
+                            </div>
+                        </Link>
+                    ))}
+                </div>
             </div>
         </div>
     );
