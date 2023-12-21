6257fa021f3a411c517b0139307edd55456dfbdb
Kyle <kcparks1234@gmail.com>
2023-12-21 12:40:11 -0800

    Added the chatbox to my dashboard

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,3 +1,19 @@
+"use client"
+
+import { MessageSquare } from "lucide-react"
+import { Card } from "@/components/ui/card"
+import { cn } from "@/lib/utils"
+
+const tools = [
+  {
+    label: "Conversations",
+    icon: MessageSquare,
+    color: "text-violet-500",
+    bgColor: "bg-violet-500/10",
+    href: "/conversation"
+  }
+]
+
 const DashboardPage = () => {
   return (
     <div>
@@ -8,9 +24,24 @@ const DashboardPage = () => {
         <p className="text-muted-foreground font-light text-sm md:text-lg text-center">
           Chat with the smartest AI - Experience the power of AI
         </p>
-        
-        </div>
-      
+
+      </div>
+
+      <div className="px-4 md:px-20 lg:px-32 space-y-4">
+        {tools.map((tool) => (
+          <Card
+            key={tool.href}
+            className="p-4 border-black/5 flex item-center justify-between hover:shadow-md transition cursor-pointer"
+          >
+            <div className="flex item-center gap-x-4">
+              <div className={cn("p-2w-fit rounded-md", tool.bgColor)}>
+                <tool.icon className={cn("w-8 h-8", tool.color)}/>
+              </div>
+            </div>
+          </Card>
+        ))}
+      </div>
+
     </div>
   )
 }
