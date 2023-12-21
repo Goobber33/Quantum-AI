df9c4e649667fee759f9a06cfd06f243bc525d88
Kyle <kcparks1234@gmail.com>
2023-12-21 13:03:27 -0800

    Added all the tools and their attributes to the dashboard

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -62,15 +62,15 @@ const Sidebar = () => {
         <div className="space-y-4 py-4 flex flex-col h-full bg-[#111827] text-white">
             <div className="px-3 py-2 flex-1">
                 <Link href="/dashboard">
-                    <div className="flex items-center pl-3 mb-14">
-                        <div className="relative w-20 h-20 mr-4">
+                    <div className="flex items-center pl-1 mb-14">
+                        <div className="relative w-20 h-20 mr-1">
                             <Image
                                 fill
                                 alt="Logo"
                                 src="/logo.png"
                             />
                         </div>
-                        <h1 className={cn("text-2xl font-bold", montserrat.className)}>Genius</h1>
+                        <h1 className={cn("text-2xl font-bold", montserrat.className)}>QuantumIQ</h1>
                     </div>
                 </Link>
                 <div className="space-y-1">
