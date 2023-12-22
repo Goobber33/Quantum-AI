0d6c9130692302cd500d71e3235623dccc820239
Kyle <kcparks1234@gmail.com>
2023-12-21 20:10:54 -0800

    Copied the conversation folders and changed the contents to work for my code generator.

---
+++ app/(dashboard)/(routes)/code/page.tsx
@@ -0,0 +1,133 @@
+"use client";
+
+import axios from "axios";
+import * as z from "zod";
+import { Heading } from "@/components/heading";
+import { Code } from "lucide-react";
+import { useForm } from "react-hook-form";
+import { formSchema } from "./constants";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Form, FormControl, FormField, FormItem } from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+import { ChatCompletionRequestMessage } from "openai";
+import { Empty } from "@/components/empty";
+import { Loader } from "@/components/loader";
+import { cn } from "@/lib/utils";
+import { UserAvatar } from "@/components/user-avatar";
+import { BotAvatar } from "@/components/bot-avatar";
+
+const CodePage = () => {
+  const router = useRouter();
+  const [messages, setMessages] = useState<ChatCompletionRequestMessage[]>([]);
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      prompt: "",
+    }
+  });
+
+  const isLoading = form.formState.isSubmitting;
+
+  const maxRetries = 3;
+  let attempts = 0;
+
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    const userMessage: ChatCompletionRequestMessage = {
+      role: "user",
+      content: values.prompt,
+    };
+    const newMessages = [...messages, userMessage];
+
+    while (attempts < maxRetries) {
+      try {
+        const response = await axios.post("/api/code", {
+          messages: newMessages,
+        });
+        setMessages(current => [...current, userMessage, response.data]);
+        form.reset();
+        break;
+      } catch (error) {
+        if (axios.isAxiosError(error) && error.response?.status === 429) {
+          attempts++;
+          console.log(`Retry attempt ${attempts}`);
+          await new Promise(resolve => setTimeout(resolve, 5000));
+        } else {
+          console.log(error);
+          break;
+        }
+      }
+    }
+  };
+
+  return (
+    <div>
+      <Heading
+        title="Code Generation"
+        description="Generate code using descriptive text."
+        icon={Code}
+        iconColor="text-green-700"
+        bgColor="bg-green-700/10"
+      />
+      <div className="px-4 lg:px-8">
+        <div>
+          <Form {...form}>
+            <form
+              onSubmit={form.handleSubmit(onSubmit)}
+              className="rounded-lg border w-full p-4 px-3 md:px-6 focus-within:shadow-sm grid grid-cols-12 gap-2"
+            >
+              <FormField
+                name="prompt"
+                render={({ field }) => (
+                  <FormItem className="col-span-12 lg:col-span-10">
+                    <FormControl className="m-0 p-0">
+                      <Input
+                        className="border-0 outline-none focuse-visible:ring-0 focus-visible:ring-transparent"
+                        disabled={isLoading}
+                        placeholder="Simple toggle button using React hooks"
+                        {...field}
+                      />
+                    </FormControl>
+                  </FormItem>
+                )}
+              />
+              <Button className="col-span-12 lg:col-span-2 w-full" disabled={isLoading}>
+                Generate
+              </Button>
+            </form>
+          </Form>
+        </div>
+        <div className="space-y-4 mt-4">
+          {isLoading && (
+            <div className="p-8 rounded-lg w-full flex items-center justify-center bg-muted">
+              <Loader />
+            </div>
+          )}
+          {messages.length === 0 && !isLoading && (
+            <Empty label="No conversation started." />
+          )}
+          <div className="flex flex-col-reverse gap-y-4">
+            {messages.map((message) => (
+              <div
+                key={message.content}
+                className={cn(
+                  "p-8 w-full flex -tems-start gap-x-8 rounded-lg",
+                  message.role === "user" ? "bg-white border border-black/10" : "bg-muted"
+                )}
+              >
+                {message.role === "user" ? <UserAvatar /> : <BotAvatar />}
+                <p className="text-sm">
+                  {message.content}
+                </p>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default CodePage;
