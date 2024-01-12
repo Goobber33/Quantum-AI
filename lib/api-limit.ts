d23dcd6cb00ea17449fd7773cd84d43149c9c985
Kyle <kcparks1234@gmail.com>
2024-01-11 19:15:13 -0800

    Finished adding an API limit by checking to see if the user has exceeded the 5 count limit

--- lib/api-limit.ts
+++ lib/api-limit.ts
@@ -10,6 +10,42 @@ export const increaseApiLimit = async () => {
         return;
     }
 
-    const userApiLimit = await prismadb.userApiLimit
+    const userApiLimit = await prismadb.userApiLimit.findUnique({
+        where: {
+            userId
+        },
+    });
 
+    if (userApiLimit) {
+        await prismadb.userApiLimit.update({
+            where: { userId: userId },
+            data: {
+                count: userApiLimit.count + 1,
+            },
+        });
+    } else {
+        await prismadb.userApiLimit.create({
+            data: { userId: userId, count: 1 }
+        });
+    }
+}
+
+export const CheckApiLimit = async () => {
+    const { userId } = auth();
+
+    if (!userId) {
+        return false;
+    }
+
+    const userApiLimit = await prismadb.userApiLimit.findUnique({
+        where: {
+            userId
+        },
+    });
+
+    if (!userApiLimit || userApiLimit.count < MAX_FREE_COUNTS) {
+        return true;
+    } else {
+        return false;
+    }
 }
