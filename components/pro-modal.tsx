044e59eecd6eddc23e7acf96e7bbe0dab1b8fb80
Kyle <kcparks1234@gmail.com>
2024-01-14 21:46:06 -0800

    Added styling to my badge in my modal

--- components/pro-modal.tsx
+++ components/pro-modal.tsx
@@ -15,7 +15,7 @@ export const ProModal = () => {
                 <DialogTitle className="flex justify-center items-center flex-col gap-y-4 pb-2">
                     <div className="flex items-center gap-x-2 font-bold py-1">
                         Upgrade to Genius
-                        <Badge className="uppercase text-sm py-1">
+                        <Badge variant="premium" className="uppercase text-sm py-1">
                             pro
                         </Badge>
                         </div>
