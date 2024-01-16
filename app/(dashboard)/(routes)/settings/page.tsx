f94c1a70456370e8319dd918e9393f9233e5a812
Kyle <kcparks1234@gmail.com>
2024-01-16 00:56:19 -0800

    Added a check for when a user has a subscription, it no longer shows the free limit bar

--- app/(dashboard)/(routes)/settings/page.tsx
+++ app/(dashboard)/(routes)/settings/page.tsx
@@ -1,4 +1,5 @@
 import { Heading } from "@/components/heading"
+import { SubscriptionButton } from "@/components/subscription-button";
 import { checkSubscription } from "@/lib/subscription"
 import { Settings } from "lucide-react"
 
@@ -19,6 +20,7 @@ const SettingsPage = async () => {
                 <div className="text-muted-foreground text-sm">
                     {isPro ? "You are subscribed to the Pro plan." : "You are not subscribed to the Pro plan."}
                 </div>
+                <SubscriptionButton isPro={isPro} />
             </div>
         </div>
     )
