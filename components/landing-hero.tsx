5b859655dc4dab82a811bb83e45e7703587e1af8
Kyle <kcparks1234@gmail.com>
2024-01-16 02:30:14 -0800

    Finished adding my desired effect on my header text using typewriter effect

--- components/landing-hero.tsx
+++ components/landing-hero.tsx
@@ -1,17 +1,33 @@
 "use client";
 
 import { useAuth } from "@clerk/nextjs";
+import TypewriterComponent from "typewriter-effect";
 
 export const LandingHero = () => {
-  const { isSignedIn } = useAuth();
+    const { isSignedIn } = useAuth();
 
-  return (
-    <div className="text-white font-bold py-36 text-center space-y-5">
-      <div className="text-4xl sm:text-5xl md:text-6xl lg:text-7xl scpace-y-5 font-extrabold">
-        <h1>
-            The Best AI Tool for
-        </h1>
-      </div>
-    </div>
-  );
+    return (
+        <div className="text-white font-bold py-36 text-center space-y-5">
+            <div className="text-4xl sm:text-5xl md:text-6xl lg:text-7xl scpace-y-5 font-extrabold">
+                <h1>
+                    The Best AI Tool for
+                </h1>
+                <div className="text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600">
+                    <TypewriterComponent
+                        options={{
+                            strings: [
+                                "Chatbot.",
+                                "Image Generation",
+                                "Video Generation",
+                                "Music Generation",
+                                "Code Generation",
+                            ],
+                            autoStart: true,
+                            loop: true,
+                        }}
+                    />
+                </div>
+            </div>
+        </div>
+    );
 };
